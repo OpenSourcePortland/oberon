dedb1c847bdd69cec3673de77335fdeccedb5e39
Duncan Miller <duncanjmiller@gmail.com>
2013-04-15 14:34:47 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oberon::Application.config.secret_token = 'f8da3f6b80beae2928b5eaac541a7cf8ec15c7285a5b81e42f320ec73f014a5bba6e03e6eaa3aa861181ee119909ea529c8bb122274bdf83e142efa5e8fbcaff'

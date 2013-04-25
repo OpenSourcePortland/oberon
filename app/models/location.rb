f52ca2c7ee05c8fd344c4f298e06473ec99c76cd
Duncan Miller <duncanjmiller@gmail.com>
2013-04-25 12:05:41 -0700

    adding coordinates and distance calculation

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,14 @@
 class Location < ActiveRecord::Base
-  attr_accessible :category, :name
+  attr_accessible :category, :name, :x, :y, :z
   
   has_many :properties
+  
+  validates :x, :y, :z, :presence => true
+  
+  def distance(destination)
+    a = self.x - destination.x
+    b = self.y - destination.y
+    c = self.z - destination.z
+    Math.sqrt(a**2 + b**2 + c**2).round
+  end
 end
---
+++ spec/models/location_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe Location do
+  before(:each) do
+    @location1 = Fabricate(:location, x: 2, y: 10, z: 4)
+    @location2 = Fabricate(:location, x: 4, y: 4, z: 10)
+    @location3 = Fabricate(:location, x: -5000, y: -10, z: -4)
+    @location4 = Fabricate(:location, x: -4, y: -4, z: -10)
+  end
+  
+  it "should not allow any nil coordintates" do
+    @bad_location = Fabricate(:location)
+    @bad_location.x = nil
+    expect{@bad_location.save!}.to raise_error
+  end
+  
+  it "should calculate the distance between two locations" do
+    @location1.distance(@location2).should == 9
+  end
+  
+  it "should calculate the distance between two negative coordinate sets" do
+    @location3.distance(@location4).should == 4996
+  end
+  
+  it "should calculate the distance between two a negative and positive coordinate set" do
+    @location2.distance(@location4).should == 23
+  end
+  
+end
+  
